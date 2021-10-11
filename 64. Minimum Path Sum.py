b3f59e81f8b9ef2dcf67ade98129099c5dc34145
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2021-10-10 22:40:06 -0400

    Dynamic Programming, Time: O(mn), Space: O(1)

---
+++ 64. Minimum Path Sum.py
@@ -0,0 +1,18 @@
+# Dynamic Programming
+# Time: O(m*n), Space: O(1)
+# Another approach: Grid Reduction using layers, but this is faster
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j != 0:
+                    grid[i][j] += grid[i][j-1];
+                if i != 0 and j == 0:
+                    grid[i][j] += grid[i-1][j];
+                if i != 0 and j != 0:
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+                    
+        return grid[m-1][n-1];

667599f27cdf4697863eb0978998551c13370d08
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2021-10-25 15:40:33 -0400

    Add Morris In order Traversal

--- 94. Binary Tree Inorder Traversal.py
+++ 94. Binary Tree Inorder Traversal.py
@@ -18,3 +18,32 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         self.inorder(root)
         return self.arr
+    
+    # Morris Inorder Traversal
+    def inorderTraversal2(self, root: Optional[TreeNode]) -> List[int]:
+        output = []
+        cur = root
+        
+        while cur:
+            print(cur.val)
+            if not cur.left:
+                output.append(cur.val)
+                cur = cur.right
+            
+            else:
+                pred = cur.left
+                while pred.right and pred.right is not cur:
+                    pred = pred.right
+                
+                # Creating the link
+                if not pred.right:
+                    pred.right = cur
+                    cur = cur.left
+                    
+                # Removing the previous link
+                elif pred.right is cur:
+                    pred.right = None
+                    output.append(cur.val)
+                    cur = cur.right
+        
+        return output

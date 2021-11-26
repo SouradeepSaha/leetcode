9c27f0f30b5437db794732cf220ed28c940f348b
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2021-11-26 18:13:32 -0500

    Use dictionary  + trie to make both operations O(n)

--- 677. Map Sum Pairs.py
+++ 677. Map Sum Pairs.py
@@ -2,7 +2,37 @@ class Node:
     def __init__(self):
         self.children = dict()
         self.val = None
+
+ # Optimized solutions using dictionary and trie
+class MapSum2:
+    def __init__(self):
+        self.root = Node()
+        self.map = dict()
+
+    
+    def insert(self, key: str, val: int) -> None:
+        root = self.root
+        diff = val-self.map.get(key, 0)
+        self.map[key] = val
+
+        for c in key:
+            if c not in root.children:
+                root.children[c] = Node()
+            
+            root.children[c].val += diff
+            root = root.children[c]
+            
+    def sum(self, prefix: str) -> int:
+        root = self.root
+        for char in prefix:
+            if char not in root.children:
+                return 0
+            root = root.children[char]
         
+        return root.val
+        return output
+
+# Unomtimized solution
 class MapSum:
     def __init__(self):
         self.root = Node()

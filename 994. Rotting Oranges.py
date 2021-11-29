004c8ef334ecea35cf4f30353efd95c167b3a4ac
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2021-11-28 21:14:43 -0500

    BFS, use moves array

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -3,34 +3,21 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        q1, q2 = deque(), deque()
+        q1, q2, time = deque(), deque(), 0
         for row in range(len(grid)):
             for col in range(len(grid[row])):
                 if grid[row][col] == 2:
                     q1.appendleft((row,col))
         
-        time = 0
+        moves = [(-1, 0), (1,0), (0,1), (0, -1)]
         while len(q1):
-            
             row, col = q1.pop()
-            if row > 0 and grid[row-1][col] == 1:
-                q2.appendleft((row-1,col))
-                grid[row-1][col] = 2
-            
-            if col > 0 and grid[row][col-1] == 1:
-                q2.appendleft((row, col-1))
-                grid[row][col-1] = 2
-            
-            if row < len(grid)-1 and grid[row+1][col] == 1:
-                q2.appendleft((row+1, col))
-                grid[row+1][col] = 2
-            
-            if col < len(grid[0])-1 and grid[row][col+1] == 1:
-                q2.appendleft((row, col+1))
-                grid[row][col+1] = 2
-            
+            for r,c in moves:
+                if 0 <= row+r < len(grid) and 0 <= col+c < len(grid[0]) and grid[row+r][col+c] == 1:
+                    q2.appendleft((row+r,col+c))
+                    grid[row+r][col+c] = 2
+                    
             if not len(q1) and len(q2):
-                print(q2)
                 time += 1
                 q1, q2 = q2, deque()
                 

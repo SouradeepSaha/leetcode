f89a4269ca7d463cd82b880a780b2ab51178d38e
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2021-12-10 01:45:57 -0500

    Dynamic Programming O(mn)

---
+++ 72. Edit Distance.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        
+        
+        for i in range(m, -1, -1):
+            for j in range(n, -1, -1):
+                if i == m:
+                    dp[i][j] = n-j
+                elif j == n:
+                    dp[i][j] = m-i
+                
+                else:
+                    if word1[i] == word2[j]:
+                        dp[i][j] = dp[i+1][j+1]
+                    else:
+                        dp[i][j] = 1+min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
+
+        return dp[0][0]

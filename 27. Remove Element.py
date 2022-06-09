e62bdf709147f39039d572ed27ee062dd9dd831f
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2022-06-09 00:50:49 -0400

    Two pointers tricky

---
+++ 27. Remove Element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left, right = 0, len(nums)
+        
+        while left < right:
+            if nums[left] != val:
+                left += 1
+            else:
+                nums[left]=nums[right-1]
+                right -= 1
+        
+        return right

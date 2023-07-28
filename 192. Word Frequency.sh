9b3373b01d6f9fc0adc6dbefd2299b29b8d0d748
Souradeep Saha <46139736+SouradeepSaha@users.noreply.github.com>
2023-07-28 19:16:02 -0400

    Delete 192. Word Frequency.sh

--- 192. Word Frequency.sh
+++
@@ -1,3 +0,0 @@
-# Read from the file words.txt and output the word frequency list to stdout.
-# tr -s : transforms all " " into "\n"
-cat words.txt | tr -s " " "\n" | sort | uniq -c | sort -r | awk '{print $2 " "$1}'
